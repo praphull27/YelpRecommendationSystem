59274b022590389d4ee4238c36ae2f6ad9440e00
Praphull Kumar <praphull27@gmail.com>
2014-05-05 16:07:15 -0700

    With some optimizations

--- reco.py
+++ reco.py
@@ -1,10 +1,8 @@
 __author__ = 'Aravindan, Praphull, Vaibhav'
 
 import json
-import ply
-import re
+import gc
 import numpy as np
-from numpy import matrix
 from numpy import linalg as LA
 
 def unique_list(seq):
@@ -12,54 +10,75 @@ def unique_list(seq):
     seen_add = seen.add
     return [ x for x in seq if x not in seen and not seen_add(x)]
 
+def low_rank_approx(u, s, v, r):
+    Ar = np.zeros((len(u), len(v)))
+    for i in xrange(r):
+        Ar += s[i] * np.outer(u.T[i], v[i])
+    return Ar
+
+gc.enable()
+
+'''
 jsonarr_users = open("Dataset/yelp_academic_dataset_user.json").readlines()
 jsonarr_reviews = open("Dataset/yelp_academic_dataset_review.json").readlines()
 jsonarr_business = open("Dataset/yelp_academic_dataset_business.json").readlines()
+'''
+'''
+jsonarr_users = open("Dataset/user_temp.json").readlines()
+jsonarr_reviews = open("Dataset/review_temp.json").readlines()
+jsonarr_business = open("Dataset/business_temp.json").readlines()
+'''
 
 users = []
 businesses = []
 
-for jsonstr in jsonarr_users:
+for jsonstr in open("Dataset/yelp_academic_dataset_user.json").readlines():
     if(jsonstr != ""):
         jsonobj = json.loads(jsonstr)
         users.append(jsonobj["user_id"])
 
 users = unique_list(users)
 
-for jsonstr in jsonarr_business:
+for jsonstr in open("Dataset/yelp_academic_dataset_business.json").readlines():
     if(jsonstr != ""):
-        jsonobj = json.loads( jsonstr )
+        jsonobj = json.loads(jsonstr)
         businesses.append(jsonobj["business_id"])
 
 businesses = unique_list(businesses)
 
+gc.collect()
+
+print "Test Before Matrix Creation"
+
 rating_matrix = np.zeros((len(users),len(businesses)), dtype=int)
 
-for jsonrev in jsonarr_reviews:
-    if(jsonrev != ""):
-        jsonobj = json.loads(jsonrev)
+for jsonstr in open("Dataset/yelp_academic_dataset_review.json").readlines():
+    if(jsonstr != ""):
+        jsonobj = json.loads(jsonstr)
         rating_matrix[users.index(jsonobj["user_id"])][businesses.index(jsonobj["business_id"])] = jsonobj["stars"]
 
-def low_rank_approx(SVD=None, A=None, r=1):
-    if not SVD:
-        SVD = LA.svd(A, full_matrices=False)
-    u, s, v = SVD
-    Ar = np.zeros((len(u), len(v)))
-    for i in xrange(r):
-        Ar += s[i] * np.outer(u.T[i], v[i])
-    return Ar
+print "Test After Matrix Creation"
 
+gc.collect()
 
-R = rating_matrix
-u, s, v = LA.svd(R, full_matrices=False)
-(m,n) = R.shape
+print "Test Before SVD"
+
+u, s, v = LA.svd(rating_matrix, full_matrices=False)
+
+print "Test After SVD"
+
+(m,n) = rating_matrix.shape
 W = np.zeros((m, n), dtype=int)
 for j in xrange(m):
-    W[j] = [1 if z > 0 else 0 for z in R[j]]
+    W[j] = [1 if z > 0 else 0 for z in rating_matrix[j]]
+
+print "Weight Mtrix Creation Successful"
 
 for i in range(1, min(m,n)-1):
-    Ar = low_rank_approx((u, s, v), r=i)
+    Ar = low_rank_approx(u, s, v, i)
     EAr = np.dot(W,Ar)
-    Er = np.subtract(R, EAr)
+    Er = np.subtract(rating_matrix, EAr)
     FN = LA.norm(Er, 'fro')
-    print "FN for rank " + str(i) + " is " + str(FN) + "\n"
+    print "FN for rank " + str(i) + " is " + str(FN)
+    gc.collect()
+
