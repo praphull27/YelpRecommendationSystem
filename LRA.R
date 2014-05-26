51138a2e9698ef22c859c84217864568fae4d548
Praphull Kumar <praphull27@gmail.com>
2014-05-25 18:21:45 -0700

    updated LRA

--- LRA.R
+++ LRA.R
@@ -1,7 +1,10 @@
 Wlocations <- which (reviews != 0, arr.ind=T)
 
+minRank <- 0
+minNorm <- 3000
+
 ptm <- proc.time()
-for (i in c(89)) {
+for (i in 90:100) {
 	u <- reviewsSVDRank100$u[,1:i]
 	D <- reviewsSVDRank100$d[1:i]
 	v <- t(reviewsSVDRank100$v[,1:i])
@@ -11,11 +14,19 @@ for (i in c(89)) {
 	remove('T', 'v')
 
 	sum <- 0
-	for (i in 1:dim(Wlocations)[1]) {
-		sum = sum + (reviews[Wlocations[i,1], Wlocations[i,2]] - LRARank[Wlocations[i,1], Wlocations[i,2]])^2
+	for (j in 1:dim(Wlocations)[1]) {
+		sum = sum + (reviews[Wlocations[j,1], Wlocations[j,2]] - LRARank[Wlocations[j,1], Wlocations[j,2]])^2
 	}
 	fNorm <- sqrt(sum)
 	cat ("Fnorm for Rank ", i, " = ", fNorm, "\n")
+	if (minNorm > fNorm) {
+		minNorm <- fNorm
+		minRank <- i
+		minLRAmatrix <- LRARank
+	}
+	remove('LRARank')
 }
 ptm <- proc.time() - ptm
 print (ptm)
+
+save.image()
